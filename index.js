5e2608609a6a6c61327d2a70b47502a1d611d7ab
Hillary Spitzer <hspitzer2@gmail.com>
2019-04-17 00:47:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 // write your code below!
 console.log('Hello, World!');
 console.log('whatever will be maybe');
+console.log( 'Hi, how are you, ' + person2 + '?' )
+console.log( 'Oh, I am doing just fine, ' + person1 + ', thanks!' )
